6ac410e4e55b87a2e8ac211077fe876f929345f4
Zanel <thanalance@gmail.com>
2024-02-12 15:53:45 -0800

    Added more classes

--- darkness.js
+++ darkness.js
@@ -3,6 +3,8 @@ window.onload = toggleDark;
 function toggleDark() {
     var img = document.querySelector("#image02 .frame img");
     var wrapper = document.querySelectorAll(".wrapper");
+    var buttons = document.querySelectorAll(".button");
+    var text = document.querySelectorAll("span.p");
     var body = document.querySelector("body");
     if ((!img) || (!wrapper)) return;
 
@@ -11,6 +13,12 @@ function toggleDark() {
     if (date.getHours() < 6 || date.getHours() > 18) {
         body.classList.add('dark');
         wrapper.forEach(element => {
+            element.classList.add('dark');
+        });
+        buttons.forEach(element => {
+            element.classList.add('dark');
+        });
+        text.forEach(element => {
             element.classList.add('dark');
         });
         img.src = "/assets/images/image01.gif";
