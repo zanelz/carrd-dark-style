954f2594f33cf3f454984120267efd2edc7ba0e9
Zanel <thanalance@gmail.com>
2024-02-12 15:34:42 -0800

    Select all wrappers

--- darkness.js
+++ darkness.js
@@ -1,16 +1,18 @@
 window.onload = toggleDark;
 
 function toggleDark() {
-var img = document.querySelector("#image02 .frame img");
-var wrapper = document.querySelector(".wrapper");
-var body = document.querySelector("body");
-if ((!img) || (!wrapper)) return;
+    var img = document.querySelector("#image02 .frame img");
+    var wrapper = document.querySelectorAll(".wrapper");
+    var body = document.querySelector("body");
+    if ((!img) || (!wrapper)) return;
 
-var date = new Date();
+    var date = new Date();
 
-if (date.getHours() < 6 || date.getHours() > 18) {
-body.classList.add('dark');
-wrapper.classList.add('dark');
-img.src = "/assets/images/image01.gif";
-}
+    if (date.getHours() < 6 || date.getHours() > 18) {
+        body.classList.add('dark');
+        wrapper.forEach(element => {
+            element.classList.add('dark');
+        });
+        img.src = "/assets/images/image01.gif";
+    }
 }
